6bc24c665069d97ce7aacbb376ab53707abc7488
jpmayoute <jeanphilippe.mayoute@gmail.com>
2024-07-05 15:26:23 +0200

    update constraints of the club form

--- src/Form/AddClubType.php
+++ src/Form/AddClubType.php
@@ -9,43 +9,40 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints\Length;
 
 class AddClubType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('club_name')
-            ->add('licence_number')
-            ->add('adress')
-            ->add('logo')
-            ->add('created_at', null, [
-                'widget' => 'single_text',
+            ->add('club_name', TextType::class, [
+                "label" => "Nom du Club",
+                'constraints' => [
+                    new NotNull()
+                ]
             ])
-            ->add('updated_at', null, [
-                'widget' => 'single_text',
+            ->add('licence_number', IntegerType::class, [
+                "label" => "Numéro de license du club",
+                'constraints' => [
+                    new NotNull(),
+                    new Length(min : 6)
+                ]
             ])
-            ->add('tournament', EntityType::class, [
-                'class' => Tournament::class,
-                'choice_label' => 'id',
-                'multiple' => true,
-            ])
-            ->add('game', EntityType::class, [
-                'class' => Game::class,
-                'choice_label' => 'id',
-            ])
-            ->add('firstclub', EntityType::class, [
-                'class' => Game::class,
-                'choice_label' => 'id',
-            ])
-            ->add('secondclub', EntityType::class, [
-                'class' => Game::class,
-                'choice_label' => 'id',
-            ])
-            ->add('winner', EntityType::class, [
-                'class' => Game::class,
-                'choice_label' => 'id',
-            ])
+            ->add('adress', TextType::class, [
+                "label" => "Adresse du club",
+                'constraints' => [
+                    new NotNull()
+                ]
+            ]) 
+            ->add('logo', UrlType::class, [
+                "label" => "Image",
+                'help' => "Url vers l'image"
+            ]) 
         ;
     }
 
