9d68b9a3f5fdb9379fe8f1f3e39d2ee9d6883363
ClementHennebelle <clement.hennebelle@oclock.school>
2024-07-05 14:41:50 +0200

    contact form created with send email to ofoot email

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $data = $form->getData();
+                $name = $data['name'];
+                $adress = $data['email'];
+                $content = $data['content'];
+
+                $from = sprintf('%s <%s>', $name, $adress);
+
+                
+                $email = (new Email())
+                    ->from($from)
+                    ->to('ofoot.contact@gmail.com')
+                    ->subject('Demande de contact')
+                    ->text($content);
+
+                    $mailer -> send($email);
+                    
+            // redirection meme page pour vider le formulaire
+            return $this->redirectToRoute('app_contact');
+            
+            }
+
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+            'formulaire' => $form->createView(),
+        ]);
+    }
+}
