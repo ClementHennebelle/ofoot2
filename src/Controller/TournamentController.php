831c872dcabb2291480743bc2eeeb5fc495d3829
BelabbasMohammadQassem <mohammadqassem.93390.b@gmail.com>
2024-07-05 15:05:03 +0200

    form tournament

--- src/Controller/TournamentController.php
+++ src/Controller/TournamentController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Tournament;
+use App\Form\TournamentType;
 use App\Repository\TournamentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -33,16 +36,27 @@ class TournamentController extends AbstractController
 
     
 
-    #[Route('/tournament/createtournoi', name: 'app_tournament_create', methods:"GET", requirements: ["id" => "\d+"])]
-    public function create(TournamentRepository $tournamentRepository): Response
+// route accessible en GET et en POST
+    #[Route('/tournament/add', name: 'app_tournament_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $tournamentCreate= $tournamentRepository->findAll();
-        return $this->render('tournament/create.html.twig', [
-            'tournamentCreate' => $tournamentCreate,
+        $tournament = new Tournament();
+        $form = $this->createForm(TournamentType::class, $tournament);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            // 3 traiter le formulaire
+            $entityManager->persist($tournament);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_tournament_read',  ['id' => $tournament->getId()], );
+        }
+
+        return $this->render('tournament/add.html.twig', [
+            'tournament' => $tournament,
+            'form' => $form,
         ]);
     }
-
-
-   
     
 }
