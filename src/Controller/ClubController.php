0c72e0d5ecfc1a0974a9ae5bd9f3090425f2da5a
ClementHennebelle <clement.hennebelle@oclock.school>
2024-07-08 13:41:44 +0200

    css des pages du back office

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -5,24 +5,69 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ClubRepository;
+use App\Entity\Club;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\AddClubType;
+
+
 
 class ClubController extends AbstractController
 {
-    #[Route('/club', name: 'app_club')]
-    public function index(): Response
+    #[Route('/club', name: 'app_club_browse', methods:"GET")]
+    public function browse(ClubRepository $clubpRepo): Response
     {
-        return $this->render('club/index.html.twig', [
-            'controller_name' => 'ClubController',
+
+        $allClubs = $clubpRepo->findAll();
+
+        return $this->render('club/browse.html.twig', [
+            'clubList' => $allClubs,
         ]);
     }
 
-     // je met ici le club
 
-     #[Route('/club/createclub', name: 'app_club_create', methods:"GET", requirements: ["id" => "\d+"])]
-     public function createclub(): Response
+     #[Route('/club/{id}', name: 'app_club_read', methods:"GET", requirements: ["id" => "\d+"])]
+     public function read(Club $club): Response
      {
-         return $this->render('club/create.html.twig', [
-             // 'controller_name' => 'TournamentController',
+         return $this->render('club/read.html.twig', [
+            'club' => $club,
          ]);
      }
+
+
+     #[Route('/club/add', name: 'app_club_add', methods:["GET", "POST"], requirements: ["id" => "\d+"])]
+     public function add(EntityManagerInterface $em, Request $request): Response
+     {
+
+        $club= new Club();
+
+        $form = $this->createForm(AddClubType::class, $club);
+        $form->setData($club);
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em->persist($club);
+
+            $em->flush();
+
+            $this->addFlash('success', 'Club ajouté avec succès');
+
+            return $this->redirectToRoute('app_club_read', ["id" => $club->getId()] );
+        }
+
+        return $this->render('club/add.html.twig', [
+            'form' => $form,
+        ]);
+
+         
+     }
+
 }
+
+
+
+    
