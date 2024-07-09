55b27ac43ba59126f0671fb05d98cfb49741e2bf
BelabbasMohammadQassem <mohammadqassem.93390.b@gmail.com>
2024-07-09 10:09:39 +0200

    duplicate

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -50,28 +50,7 @@ class RegistrationFormType extends AbstractType
                         'message' => 'Vous devez accepter les conditions générales d\utilisation.',
                     ]),
                 ],
-            ])
-           
-           
-            
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
