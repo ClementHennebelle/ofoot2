46eee92eb566390732923861f5eed1d5860d7b6d
BelabbasMohammadQassem <mohammadqassem.93390.b@gmail.com>
2024-07-10 14:40:29 +0200

    pull

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\TournamentRepository;
+use App\Repository\UserRepostory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -57,8 +59,7 @@ class MainController extends AbstractController
                         return $this->render('main/cgu.html.twig', [
                             // 'tournaments' => $tournaments,
                         ]);
-                    } 
-
+                    }
 
 
     
