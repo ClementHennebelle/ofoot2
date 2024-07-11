df6b1ae827240e116dcb1cee5f245a479b61aa71
ClementHennebelle <clement.hennebelle@oclock.school>
2024-07-11 15:20:18 +0200

    update account + account/home, dynamisation account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -11,16 +11,40 @@ class AccountController extends AbstractController
     #[Route('/account', name: 'app_account')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        $clubName = $user->getClub() ? $user->getClub()->getClubName() : 'Aucun club associé';
+        $accountInfo = [
+            'lastname' => $user->getLastname(),
+            'firstname' => $user->getFirstname(),
+            'email' => $user->getEmail(),
+            'clubName' => $clubName,
+        ];
+
         return $this->render('account/account.html.twig', [
-            'controller_name' => 'AccountController',
+            'accountInfo' => $accountInfo,
         ]);
     }
 
     #[Route('/account/home', name: 'app_account_home')]
     public function home(): Response
     {
+        $user = $this->getUser();
+        $clubName = $user->getClub() ? $user->getClub()->getClubName() : 'Aucun club associé';
+        $clubLicenceNumber = $user->getClub() ? $user->getClub()->getLicenceNumber() : 'Aucun numéro de licence associé';
+        $clubDateCreation = $user->getClub() ? $user->getClub()->getCreatedAt() : 'Aucun club associé';
+        $clubLogo = $user->getClub() ? $user->getClub()->getLogo() : 'Aucun logo associé';
+        $accountInfo = [
+            'lastname' => $user->getLastname(),
+            'firstname' => $user->getFirstname(),
+            'email' => $user->getEmail(),
+            'clubName' => $clubName,
+            'clubLicenceNumber' => $clubLicenceNumber,
+            'clubDateCreation' => $clubDateCreation,
+            'clubLogo' => $clubLogo,   
+        ];
         return $this->render('account/accounthome.html.twig', [
             'controller_name' => 'AccountController',
+            'accountInfo' => $accountInfo
         ]);
     }
 
