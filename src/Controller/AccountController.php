9d0ec76b49ef8434d4aa2a33185931ae19b3baa3
ClementHennebelle <clement.hennebelle@oclock.school>
2024-07-12 11:54:12 +0200

    debug

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,37 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\Club;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
 {
     #[Route('/account', name: 'app_account')]
     public function index(): Response
-    {
-        $user = $this->getUser();
-        $clubName = $user->getClub() ? $user->getClub()->getClubName() : 'Aucun club associé';
-        $accountInfo = [
-            'lastname' => $user->getLastname(),
-            'firstname' => $user->getFirstname(),
-            'email' => $user->getEmail(),
-            'clubName' => $clubName,
-        ];
+{
+    $user = $this->getUser();
 
-        return $this->render('account/account.html.twig', [
-            'accountInfo' => $accountInfo,
-        ]);
+    // Vérifier si l'utilisateur est connecté
+    if (!$user) {
+        // Gérer le cas où aucun utilisateur n'est connecté
+        return $this->redirectToRoute('app_login'); // Rediriger vers la page de connexion par exemple
     }
+
+    // Si l'utilisateur est connecté, récupérer les informations du compte
+    $clubName = $user->getClub() ? $user->getClub()->getClubName() : 'Aucun club associé';
+    $accountInfo = [
+        'lastname' => $user->getLastname(),
+        'firstname' => $user->getFirstname(),
+        'email' => $user->getEmail(),
+        'clubName' => $clubName,
+    ];
+
+    return $this->render('account/account.html.twig', [
+        'accountInfo' => $accountInfo,
+    ]);
+}
 
     #[Route('/account/home', name: 'app_account_home')]
     public function home(): Response
     {
         $user = $this->getUser();
-        $clubName = $user->getClub() ? $user->getClub()->getClubName() : 'Aucun club associé';
-        $clubLicenceNumber = $user->getClub() ? $user->getClub()->getLicenceNumber() : 'Aucun numéro de licence associé';
-        $clubDateCreation = $user->getClub() ? $user->getClub()->getCreatedAt() : 'Aucun club associé';
-        $clubLogo = $user->getClub() ? $user->getClub()->getLogo() : 'Aucun logo associé';
+        $club = $user->getClub();
+        $clubName = $club ? $club->getClubName() : 'Aucun club associé';
+        $clubLicenceNumber = $club ? $club->getLicenceNumber() : 'Aucun numéro de licence associé';
+        $clubDateCreation = $club ? $club->getCreatedAt()->format('d-m-Y') : 'Aucun club associé';
+        $clubLogo = $club ? $club->getLogo() : 'Aucun logo associé';
+
         $accountInfo = [
             'lastname' => $user->getLastname(),
             'firstname' => $user->getFirstname(),
@@ -40,19 +54,44 @@ class AccountController extends AbstractController
             'clubName' => $clubName,
             'clubLicenceNumber' => $clubLicenceNumber,
             'clubDateCreation' => $clubDateCreation,
-            'clubLogo' => $clubLogo,   
+            'clubLogo' => $clubLogo,
         ];
+
         return $this->render('account/accounthome.html.twig', [
             'controller_name' => 'AccountController',
-            'accountInfo' => $accountInfo
+            'accountInfo' => $accountInfo,
         ]);
     }
 
-    #[Route('/account/addclub', name: 'app_account_addclub')]
-    public function addClub(): Response
+    #[Route('/account/addclub', name: 'app_account_addclub', methods: ['GET', 'POST'])]
+    public function addClub(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $user = $this->getUser();
+
+        // Récupération de tous les clubs pour afficher dans le formulaire
+        $clubs = $entityManager->getRepository(Club::class)->findAll();
+
+        if ($request->isMethod('POST')) {
+            $clubId = $request->request->get('club');
+
+            if ($clubId) {
+                $club = $entityManager->getRepository(Club::class)->find($clubId);
+
+                if ($club) {
+                    // Associer l'utilisateur au club sélectionné
+                    $user->setClub($club);
+                    $entityManager->flush();
+
+                    $this->addFlash('success', 'Votre compte a été rattaché au club avec succès.');
+                    return $this->redirectToRoute('app_account');
+                }
+            }
+
+            $this->addFlash('error', 'Veuillez sélectionner un club valide.');
+        }
+
         return $this->render('account/addclubaccount.html.twig', [
-            'controller_name' => 'AccountController',
+            'clubs' => $clubs,
         ]);
     }
 }
